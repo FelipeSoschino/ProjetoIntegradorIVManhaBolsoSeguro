830b4d7b534e387afba8323da7c6c76c06eefa18
FelipeSoschino <felipesoschinorego@yahoo.com.br>
2026-03-06 11:31:56 -0300

    Primeiro Commit

---
+++ src/main/java/com/senac/bolsoseguro/BolsoseguroApplication.java
@@ -0,0 +1,13 @@
+package com.senac.bolsoseguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsoseguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsoseguroApplication.class, args);
+	}
+
+}
