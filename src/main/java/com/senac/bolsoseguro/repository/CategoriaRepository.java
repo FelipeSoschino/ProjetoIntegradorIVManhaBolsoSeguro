b0a20c5876c114051e876568f0b72bea7edc631e
FelipeSoschino <felipesoschinorego@yahoo.com.br>
2026-03-10 09:47:28 -0300

    Terceiro Commit, Repostories e Services criados e construtores adicionados apenas.

---
+++ src/main/java/com/senac/bolsoseguro/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.senac.bolsoseguro.repository;
+
+import com.senac.bolsoseguro.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
